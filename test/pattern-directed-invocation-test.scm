a345f7fc1688dd83778bde40c8492c0f3be882d6
Alexey Radul <axch@mit.edu>
2010-12-13 10:07:58 +0000

    Converting inline examples into unit tests.

--- test/pattern-directed-invocation-test.scm
+++ test/pattern-directed-invocation-test.scm
@@ -1,36 +1,41 @@
-#|
- (define quad
-   (make-pattern-operator
-    (list
-     (rule 
-      `((? a) (? b) (? c) (? x))
-      (+ (* a (expt x 2))
-	 (* b x)
-	 c))
+(in-test-group
+ pattern-directed-invocation
+ 
+ (define-test (quad-test)
+   (interaction
+    (define quad
+      (make-pattern-operator
+       (list
+	(rule 
+	 `((? a) (? b) (? c) (? x))
+	 (+ (* a (expt x 2))
+	    (* b x)
+	    c))
 
+	(rule
+	 `((? a) (? x) (? x) + (? b) (? x) + (? c))
+	 (+ (* a (expt x 2))
+	    (* b x)
+	    c)))))
+
+    (quad 1 2 3 4)
+    (produces 27)
+
+    (quad 1 4 4 '+ 2 4 '+ 3)
+    (produces 27)))
+
+ (define-test (frob-test)
+   (interaction
+    (define frob
+      (make-pattern-operator))
+
+    (attach-rule! frob
      (rule
-      `((? a) (? x) (? x) + (? b) (? x) + (? c))
-      (+ (* a (expt x 2))
-	 (* b x)
-	 c)))))
+      '(a (?? x) (?? y) (?? x) c)
+      (and (<= (length y) 2)
+	   y)))
 
- (quad 1 2 3 4)
- ;Value: 27
+    (apply frob '(a b b b b b b c))
+    (produces '(b b)))))
 
- (quad 1 4 4 '+ 2 4 '+ 3)
- ;Value: 27
-|#
 
-#|
- (define frob
-   (make-pattern-operator))
-
- (attach-rule! frob
-  (rule
-   '(a (?? x) (?? y) (?? x) c)
-   (and (<= (length y) 2)
-        y)))
-
- (apply frob '(a b b b b b b c))
- ;Value: (b b)
-|#
