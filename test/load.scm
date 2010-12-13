c71e1f55cc86d6c13d83f78dddaed2ec84a078cc
Alexey Radul <axch@mit.edu>
2010-12-13 10:02:33 +0000

    Starting with the unit testing framework.

---
+++ test/load.scm
@@ -0,0 +1,18 @@
+(define (self-relatively thunk)
+  (if (current-eval-unit #f)
+      (with-working-directory-pathname
+       (directory-namestring (current-load-pathname))
+       thunk)
+      (thunk)))
+
+(define (load-relative filename)
+  (self-relatively (lambda () (load filename))))
+
+(load-relative "../../testing/load")
+(load-relative "pattern-directed-invocation-test")
+
+(define (run-tests-and-exit)
+  (let ((v (show-time run-registered-tests)))
+    (newline)
+    (flush-output)
+    (%exit v)))
