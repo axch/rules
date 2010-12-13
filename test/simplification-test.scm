352f9e4e22add6b17dc2db3037260cf5e9633340
Alexey Radul <axch@mit.edu>
2010-12-13 11:29:13 +0000

    More unit tests, this time of the simplification systems.

--- test/simplification-test.scm
+++ test/simplification-test.scm
@@ -13,7 +13,14 @@
    (equal? 0 (simplify-algebra '(+)))
    (equal? #t (simplify-logic '(and)))
    (equal? #f (simplify-logic '(or)))
-   )
+   (equal?
+   '(* 3 x)
+   (simplify-algebra '(+ (* 3 (+ x 1)) -3
+			 (* y (+ 1 2 -3) z))))
+   (equal?
+   '(/ (* r1 r2) (+ r1 r2))
+   ((compose simplify-quotient ->quotient-of-sums)
+    '(/ 1 (+ (/ 1 r1) (/ 1 r2))))))
 
  (define associate-addition
    (rule '(+ (? a) (+ (? b) (? c)))
@@ -33,6 +40,59 @@
     '(+ 2 3)
     (sort-numbers '(+ 3 2)))
    (assert-false (sort-numbers '(+ 2 3))))
+ (define-test (parametric-rule-smoke)
+   (assert-equal
+    #f
+    (simplify-ors '(or))))
+
+ (define-test (negation-pushing-smoke)
+   (assert-equal
+    '(and (not a) (not b))
+    (simplify-negations '(not (or a b)))))
+
+ (define-test (or-pushing)
+   (assert-equal
+    '(and (or (not (< -1/4 (- x2 x1)))
+	      (and (not (< (- x2 x1) 1/4)) (not (< (- x2 x1) 0)))
+	      (and (< (- x2 x1) 1/4) (not (< (- x2 x1) 0)))
+	      (and (< -1/4 (- x2 x1)) (< (- x2 x1) 0)))
+	  (or (< (- x2 x1) 0)
+	      (and (not (< (- x2 x1) 1/4)) (not (< (- x2 x1) 0)))
+	      (and (< (- x2 x1) 1/4) (not (< (- x2 x1) 0)))
+	      (and (< -1/4 (- x2 x1)) (< (- x2 x1) 0))))
+    (push-or-through-and
+     '(or (and (not (< -1/4 (- x2 x1)))
+	       (< (- x2 x1) 0))
+	  (and (not (< (- x2 x1) 1/4))
+	       (not (< (- x2 x1) 0)))
+	  (and (< (- x2 x1) 1/4)
+	       (not (< (- x2 x1) 0)))
+	  (and (< -1/4 (- x2 x1))
+	       (< (- x2 x1) 0))))))
+
+ (define-test (cnf)
+   (assert-true
+    (->conjunctive-normal-form
+     '(or (and (not (< -1/4 (- x2 x1)))
+	       (< (- x2 x1) 0))
+	  (and (not (< (- x2 x1) 1/4))
+	       (not (< (- x2 x1) 0)))
+	  (and (< (- x2 x1) 1/4)
+	       (not (< (- x2 x1) 0)))
+	  (and (< -1/4 (- x2 x1))
+	       (< (- x2 x1) 0))))))
+
+ (define-test (more-cnf)
+   (assert-equal
+    '(and (or a b)
+	  (or a (not b))
+	  (or b (not a))
+	  (or (not a) (not b)))
+    (->conjunctive-normal-form
+     '(and (or a b)
+	   (or a (not b))
+	   (or (not a) b)
+	   (or (not a) (not b))))))
 
  (define-test (scanning-for-duplicates)
    (define find-consecutive-dups
