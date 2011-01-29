61c57a3582967d7c7d51539fdccbcc52bca1f863
Alexey Radul <axch@mit.edu>
2011-01-29 18:03:23 +0000

    Removing tabs.

--- simplifiers.scm
+++ simplifiers.scm
@@ -6,69 +6,69 @@
 
 (define (nullary-replacement operator value)
   (rule `(,operator)
-	(succeed value)))
+        (succeed value)))
 
 (define (unary-elimination operator)
   (rule `(,operator (? a))
-	(succeed a)))
+        (succeed a)))
 
 (define (constant-elimination operator constant)
   (rule `(,operator ,constant (?? x))
-	`(,operator ,@x)))
+        `(,operator ,@x)))
 
 (define (constant-promotion operator constant)
   (rule `(,operator ,constant (?? x))
-	(succeed constant)))
+        (succeed constant)))
 
 (define (associativity operator)
   (rule `(,operator (?? a) (,operator (?? b)) (?? c))
-	#; `(,operator ,@a ,@b ,@c) ; Too slow to do them one at a time
-	(append-map (lambda (item)
-		      (if (and (pair? item)
-			       (eq? operator (car item)))
-			  (cdr item)
-			  (list item)))
-		    `(,operator ,@a ,@b ,@c))))
+        #; `(,operator ,@a ,@b ,@c) ; Too slow to do them one at a time
+        (append-map (lambda (item)
+                      (if (and (pair? item)
+                               (eq? operator (car item)))
+                          (cdr item)
+                          (list item)))
+                    `(,operator ,@a ,@b ,@c))))
 
 (define (sorted? lst <)
   ;; Specifically, I am testing that a stable sort of lst by < will
   ;; not change anything, that is, that there are no reversals where a
   ;; later item is < an earlier one.
   (cond ((not (pair? lst)) #t)
-	((not (pair? (cdr lst))) #t)
-	((< (cadr lst) (car lst)) #f)
-	(else (sorted? (cdr lst) <))))
+        ((not (pair? (cdr lst))) #t)
+        ((< (cadr lst) (car lst)) #f)
+        (else (sorted? (cdr lst) <))))
 
 (define (commutativity operator)
   ;; Flipping one at a time is bubble sort
   #;
   (rule `(,operator (?? a) (? y) (? x) (?? b))
-	`(,operator ,@a ,x ,y ,@b))
+        `(,operator ,@a ,x ,y ,@b))
   ;; Finding a pair out of order and sorting is still quadratic,
   ;; because the matcher matches N times, and each requires
   ;; constructing the segments so they can be handed to the handler
   ;; (laziness would help).
   #;
   (rule `(,operator (?? a) (? y) (? x) (?? b))
-	(and (expr<? x y)
-	     `(,operator ,@(sort `(,@a ,x ,y ,@b) expr<?))))
+        (and (expr<? x y)
+             `(,operator ,@(sort `(,@a ,x ,y ,@b) expr<?))))
   (rule `(,operator (?? terms))
-	(and (not (sorted? terms expr<?))
-	     `(,operator ,@(sort terms expr<?)))))
+        (and (not (sorted? terms expr<?))
+             `(,operator ,@(sort terms expr<?)))))
 
 (define (idempotence operator)
   (define (remove-consecutive-duplicates lst)
     (cond ((null? lst)
-	   '())
-	  ((null? (cdr lst))
-	   lst)
-	  ((equal? (car lst) (cadr lst))
-	   (remove-consecutive-duplicates (cdr lst)))
-	  (else
-	   (cons (car lst) (remove-consecutive-duplicates (cdr lst))))))
+           '())
+          ((null? (cdr lst))
+           lst)
+          ((equal? (car lst) (cadr lst))
+           (remove-consecutive-duplicates (cdr lst)))
+          (else
+           (cons (car lst) (remove-consecutive-duplicates (cdr lst))))))
   (rule `(,operator (?? a) (? x) (? x) (?? b))
-	#; `(or ,@a ,x ,@b) ; One at a time is too slow
-	`(,operator ,@(remove-consecutive-duplicates `(,@a ,x ,@b)))))
+        #; `(or ,@a ,x ,@b) ; One at a time is too slow
+        `(,operator ,@(remove-consecutive-duplicates `(,@a ,x ,@b)))))
 
 ;;;; Some algebraic simplification rules
 
@@ -81,7 +81,7 @@
     (unary-elimination '+)
     (constant-elimination '+ 0)
     (rule `(+ (? x ,number?) (? y ,number?) (?? z))
-	  `(+ ,(+ x y) ,@z))
+          `(+ ,(+ x y) ,@z))
     (associativity '+)
     (commutativity '+)
 
@@ -96,7 +96,7 @@
     (constant-elimination '* 1)
     (constant-promotion '* 0)
     (rule `(* (? x ,number?) (? y ,number?) (?? z))
-	  `(* ,(* x y) ,@z))
+          `(* ,(* x y) ,@z))
     (associativity '*)
     (commutativity '*) ;; TODO be able to turn this off?
 
@@ -104,16 +104,16 @@
 
 (define distributive-law
   (rule `(* (?? a) (+ (?? b)) (?? c))
-	`(+ ,@(map (lambda (x)
-		     (simplify-products
-		      `(* ,@a ,x ,@c)))
-		   b))))
+        `(+ ,@(map (lambda (x)
+                     (simplify-products
+                      `(* ,@a ,x ,@c)))
+                   b))))
 
 (define simplify-algebra
   (iterate-until-stable
    (compose (rule-simplifier (list distributive-law))
-	    simplify-sums
-	    simplify-products)))
+            simplify-sums
+            simplify-products)))
 
 (define simplify-quotient
   (rule-simplifier
@@ -123,15 +123,15 @@
     (rule `(/ 0 (? d)) 0)
 
     (rule `(/ 1 (/ (? n) (? d)))
-	  `(/ ,d ,n))
+          `(/ ,d ,n))
 
     (rule `(/ (? n) (? d))
-	  (let ((g (g:gcd n d)))
-	    (and (not (= g 1))
-		 (let ((nn (g:divide n g))
-		       (dd (g:divide d g)))
-		   (simplify-quotient
-		    `(/ ,nn ,dd))))))
+          (let ((g (g:gcd n d)))
+            (and (not (= g 1))
+                 (let ((nn (g:divide n g))
+                       (dd (g:divide d g)))
+                   (simplify-quotient
+                    `(/ ,nn ,dd))))))
 
     )))
 
@@ -148,29 +148,29 @@
 
     ;; Same denominator
     (rule `(+ (?? a1) (/ (? n1) (? d)) (?? a2) (/ (? n2) (? d)) (?? a3))
-	  (simplify-sums
-	   `(+ ,(simplify-quotient
-		 `(/ ,(simplify-sums `(+ ,n1 ,n2)) ,d))
-	       ,@a1 ,@a2 ,@a3)))
+          (simplify-sums
+           `(+ ,(simplify-quotient
+                 `(/ ,(simplify-sums `(+ ,n1 ,n2)) ,d))
+               ,@a1 ,@a2 ,@a3)))
 
     ;; General Case
     (rule `(+ (?? a1) (/ (? n1) (? d1)) (?? a2) (/ (? n2) (? d2)) (?? a3))
-	  (simplify-sums
-	   `(+ ,(simplify-quotient
-		 `(/ ,(simplify-sums
-		       `(+ ,(simplify-products `(* ,n1 ,d2))
-			   ,(simplify-products `(* ,n2 ,d1))))
-		     ,(simplify-products `(* ,d1 ,d2))))
-	       ,@a1 ,@a2 ,@a3)))
+          (simplify-sums
+           `(+ ,(simplify-quotient
+                 `(/ ,(simplify-sums
+                       `(+ ,(simplify-products `(* ,n1 ,d2))
+                           ,(simplify-products `(* ,n2 ,d1))))
+                     ,(simplify-products `(* ,d1 ,d2))))
+               ,@a1 ,@a2 ,@a3)))
 
     ;; Other terms
     (rule `(+ (?? a1) (/ (? n) (? d)) (?? a2))
-	  (simplify-quotient
-	   `(/ ,(simplify-sums
-		 `(+ ,n
-		     ,(simplify-products
-		       (* ,d ,(simplify-sums `(+ ,@a1 ,@a2))))))
-	       ,d)))
+          (simplify-quotient
+           `(/ ,(simplify-sums
+                 `(+ ,n
+                     ,(simplify-products
+                       (* ,d ,(simplify-sums `(+ ,@a1 ,@a2))))))
+               ,d)))
 
     )))
 
@@ -179,10 +179,10 @@
    (list
 
     (rule `(/ (+ (?? as)) (? d))
-	  `(+ ,@(map (lambda (n)
-		       (simplify-quotient
-			`(/ ,n ,d)))
-		     as)))
+          `(+ ,@(map (lambda (n)
+                       (simplify-quotient
+                        `(/ ,n ,d)))
+                     as)))
 
     )))
 
@@ -191,11 +191,11 @@
    (list
 
     (rule `(expt (? a ,number?) (? b ,number?))
-	  (s:expt a b))
+          (s:expt a b))
 
     (rule `(expt (? b) 1) b)
-    (rule `(expt (? b) -1) `(/ 1 b))	; Do we want this?
-    (rule `(expt 0 (? e)) 0)		; Needs to be positive
+    (rule `(expt (? b) -1) `(/ 1 b))    ; Do we want this?
+    (rule `(expt 0 (? e)) 0)            ; Needs to be positive
     (rule `(expt 1 (? e)) 1)
 
     )))
@@ -204,7 +204,7 @@
   (rule-simplifier
    (list
     (rule `(- (? x) (? y) (?? z))
-	  `(+ ,x (* -1 (+ ,y ,@z))))
+          `(+ ,x (* -1 (+ ,y ,@z))))
     (rule `(- (? x)) `(* -1 ,x))
     )))
 
@@ -213,10 +213,10 @@
    (list
 
     (rule `(expt (? x) (? n ,exact-integer? ,positive?))
-	  `(* ,@(make-list n x)))
+          `(* ,@(make-list n x)))
 
     (rule `(expt (? x) (? n ,exact-integer? ,negative?))
-	  `(/ 1 (* ,@(make-list n x))))
+          `(/ 1 (* ,@(make-list n x))))
 
     )))
 
@@ -225,19 +225,19 @@
    (list
 
     (rule `(* (?? f1) (? x) (? x) (?? f2))
-	  `(* ,@f1 (expt x 2) ,@f2))
+          `(* ,@f1 (expt x 2) ,@f2))
     
     (rule `(expt (expt (? x) (? n)) (? m))
-	  `(expt x (* ,n ,m)))
+          `(expt x (* ,n ,m)))
 
     (rule `(* (?? f1) (? x) (expt (? x) (? n)) (?? f2))
-	  `(* ,@f1 (expt x (+ ,n 1)) ,@f2))
+          `(* ,@f1 (expt x (+ ,n 1)) ,@f2))
     
     (rule `(* (?? f1) (expt (? x) (? n)) (? x) (?? f2))
-	  `(* ,@f1 (expt x (+ ,n 1)) ,@f2))
+          `(* ,@f1 (expt x (+ ,n 1)) ,@f2))
 
     (rule `(* (?? f1) (expt (? x) (? n)) (expt (? x) (? m)) (?? f2))
-	  `(* ,@f1 (expt x (+ ,n ,m)) ,@f2))
+          `(* ,@f1 (expt x (+ ,n ,m)) ,@f2))
 
     )))
 
@@ -253,14 +253,14 @@
     (rule `(not #f) (succeed #t))
     
     (rule `(not (or (?? terms)))
-	  `(and ,@(map (lambda (term)
-			 `(not ,term))
-		       terms)))
+          `(and ,@(map (lambda (term)
+                         `(not ,term))
+                       terms)))
 
     (rule `(not (and (?? terms)))
-	  `(or ,@(map (lambda (term)
-			`(not ,term))
-		      terms)))
+          `(or ,@(map (lambda (term)
+                        `(not ,term))
+                      terms)))
 
     )))
 
@@ -277,10 +277,10 @@
     (idempotence 'or)
 
     (rule `(or (?? stuff) (? a) (?? more-stuff) (not (? a)) (?? even-more-stuff))
-	  (succeed #t))
+          (succeed #t))
 
     (rule `(or (?? stuff) (not (? a)) (?? more-stuff) (? a) (?? even-more-stuff))
-	  (succeed #t))
+          (succeed #t))
 
     )))
 
@@ -297,18 +297,18 @@
     (idempotence 'and)
 
     (rule `(and (?? stuff) (? a) (?? more-stuff) (not (? a)) (?? even-more-stuff))
-	  (succeed #f))
+          (succeed #f))
 
     (rule `(and (?? stuff) (not (? a)) (?? more-stuff) (? a) (?? even-more-stuff))
-	  (succeed #f))
+          (succeed #f))
 
     )))
 
 (define push-or-through-and
   (rule `(or (?? or-terms-1) (and (?? and-terms)) (?? or-terms-2))
-	`(and ,@(map (lambda (and-term)
-		       `(or ,@or-terms-1 ,and-term ,@or-terms-2))
-		     and-terms))))
+        `(and ,@(map (lambda (and-term)
+                       `(or ,@or-terms-1 ,and-term ,@or-terms-2))
+                     and-terms))))
 
 ;;; TODO Implement subsumption, and then implement resolution propertly.
 
@@ -316,24 +316,24 @@
 ;;; consequences, and they remove the resolvees prematurely.
 ;; (define resolution-1
 ;;   (rule `(and (?? and-terms-1) (or (?? or-1-terms-1) (? a) (?? or-1-terms-2))
-;; 	     (?? and-terms-2) (or (?? or-2-terms-1) (not (? a)) (?? or-2-terms-2))
-;; 	     (?? and-terms-3))
-;; 	`(and ,@and-terms-1 (or ,@or-1-terms-1 ,@or-1-terms-2 ,@or-2-terms-1 ,@or-2-terms-2)
-;; 	      ,@and-terms-2 ,@and-terms-3)))
+;;           (?? and-terms-2) (or (?? or-2-terms-1) (not (? a)) (?? or-2-terms-2))
+;;           (?? and-terms-3))
+;;      `(and ,@and-terms-1 (or ,@or-1-terms-1 ,@or-1-terms-2 ,@or-2-terms-1 ,@or-2-terms-2)
+;;            ,@and-terms-2 ,@and-terms-3)))
 
 ;; (define resolution-2
 ;;   (rule `(and (?? and-terms-1) (or (?? or-1-terms-1) (not (? a)) (?? or-1-terms-2))
-;; 	     (?? and-terms-2) (or (?? or-2-terms-1) (? a) (?? or-2-terms-2))
-;; 	     (?? and-terms-3))
-;; 	`(and ,@and-terms-1 (or ,@or-1-terms-1 ,@or-1-terms-2 ,@or-2-terms-1 ,@or-2-terms-2)
-;; 	      ,@and-terms-2 ,@and-terms-3)))
+;;           (?? and-terms-2) (or (?? or-2-terms-1) (? a) (?? or-2-terms-2))
+;;           (?? and-terms-3))
+;;      `(and ,@and-terms-1 (or ,@or-1-terms-1 ,@or-1-terms-2 ,@or-2-terms-1 ,@or-2-terms-2)
+;;            ,@and-terms-2 ,@and-terms-3)))
 
 (define ->conjunctive-normal-form
   (compose
    (iterate-until-stable
     (compose (rule-simplifier (list push-or-through-and))
-	     simplify-ands
-	     simplify-ors))
+             simplify-ands
+             simplify-ors))
    simplify-negations))
 
 ;; (define do-resolution
