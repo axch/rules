da7ee69c299224076ce608e65cb3f9dd55feb9be
Alexey Radul <axch@mit.edu>
2013-05-22 23:30:24 -0400

    More concise explanations are less repetitive wrt the README.

--- rules.scm
+++ rules.scm
@@ -7,53 +7,45 @@
 ;;; under the terms of the GNU Affero General Public License as
 ;;; published by the Free Software Foundation; either version 3 of the
 ;;; License, or (at your option) any later version.
-;;; 
+;;;
 ;;; This code is distributed in the hope that it will be useful,
 ;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;;; GNU General Public License for more details.
-;;; 
+;;;
 ;;; You should have received a copy of the GNU Affero General Public
 ;;; License along with Rules; if not, see
 ;;; <http://www.gnu.org/licenses/>.
 
 (declare (usual-integrations))
 
-;;; A rule, in this terminology, is a pattern and a handler.  The
-;;; pattern determines the applicability of the rule and the match
-;;; bindings that enable it, and the handler can compute an arbitrary
-;;; value from them.  Once constructed, a rule is a procedure that
-;;; accepts a datum, and returns either the datum if the pattern
-;;; doesn't match or the value of the handler when applied to the
-;;; dictionary if it does.  The input datum is used as the sentinel
-;;; value for failure because in the context of term rewriting,
-;;; succeeding with the input as the answer is equivalent to failing.
+;;;; Rules
+
+;;; A rule is a pattern and a handler.  The pattern determines the
+;;; applicability of the rule, and the match bindings that enable said
+;;; applicability, and the handler can compute an arbitrary value from
+;;; them.  Once constructed, a rule is a procedure that accepts a
+;;; datum, and returns either the datum if the pattern doesn't match
+;;; or the value of the handler when applied to the dictionary if it
+;;; does.  The input datum is used as the sentinel value for failure
+;;; because in the context of term rewriting, succeeding with the
+;;; input as the answer is equivalent to failing.
 
 ;;; The handler can reject a putative match by returning #f, which
 ;;; causes backtracking into the matcher, and may cause the handler to
 ;;; be called again with different bindings.  If the handler always
 ;;; returns #f, the rule may fail even though its pattern matched.
-;;; The handler can force success with an arbitrary object (including
-;;; #f) by returning the result of calling `succeed' on the value to
-;;; return.
-
-;;; For situations where it is desirable to distinguish rule failure
-;;; from success with the input, the rule procedure accepts an
-;;; optional second argument to use as a token to indicate failure.
-;;; If the second argument is given, it is returned on failure, and
-;;; the input datum (along with all other objects) indicates success.
-;;; The token is not stored across invocations and is not passed to
-;;; handlers.  It is up to the caller to make sure that this token is
-;;; unique.
 
 (define (make-rule pattern handler)
   (if (accepts-variables? handler)
       (make-rule pattern (accept-dictionary
 			  handler (match:pattern-names pattern)))
       (let ((combinator (match:->combinators pattern)))
+        ;; The value to return on failure can be overridden to
+        ;; distinguish idempotent success from actual failure, should
+        ;; that be important.
 	(lambda (data #!optional fail-token)
-	  (if (default-object? fail-token)
-	      (set! fail-token data))
+	  (if (default-object? fail-token) (set! fail-token data))
 	  (interpret-success
            (or (combinator data '() handler)
                ;; Not fail-token because it might be a success object
@@ -75,6 +67,10 @@
 		     name dict))))
        (let ((argument-list (map matched-value argl)))
          (apply f argument-list))))))
+
+;;; The handler can force success with an arbitrary object (including
+;;; #f) by returning the result of calling `succeed' on the value to
+;;; return.
 
 (define-structure success
   value)
@@ -106,7 +102,7 @@
    (lambda (form use-env)
      (let ((pattern (cadr form))
 	   (handler-body (caddr form)))
-       `(make-rule 
+       `(make-rule
 	 ,(close-syntax pattern use-env)
 	 ,(compile-handler handler-body use-env
 			   (match:pattern-names pattern)))))))
