077c1aff52795e5ce1c927f409e7cfecb214735b
Alexey Radul <axch@mit.edu>
2013-05-23 09:54:23 -0400

    The expression sort goes with the simplifiers, not the term rewriting system.

--- term-rewriting.scm
+++ term-rewriting.scm
@@ -109,35 +109,3 @@
                   (on-expression answer))))
           (on-expression answer))))
   on-expression)
-
-
-(define (list<? x y)
-  (let ((nx (length x)) (ny (length y)))
-    (cond ((< nx ny) #t)
-	  ((> nx ny) #f)
-	  (else
-	   (let lp ((x x) (y y))
-	     (cond ((null? x) #f)	; same
-		   ((expr<? (car x) (car y)) #t)
-		   ((expr<? (car y) (car x)) #f)
-		   (else (lp (cdr x) (cdr y)))))))))
-
-(define expr<?
-  (make-entity
-   (lambda (self x y)
-     (let per-type ((types (entity-extra self)))
-       (if (null? types)
-	   (error "Unknown expression type -- expr<?" x y)
-	   (let ((predicate? (caar types))
-		 (comparator (cdar types)))
-	     (cond ((predicate? x)
-		    (if (predicate? y)
-			(comparator x y)
-			#t))
-		   ((predicate? y) #f)
-		   (else (per-type (cdr types))))))))
-   `((,null?    . ,(lambda (x y) #f))
-     (,boolean? . ,(lambda (x y) (and (eq? x #t) (eq? y #f))))
-     (,number?  . ,<)
-     (,symbol?  . ,symbol<?)
-     (,list?    . ,list<?))))
